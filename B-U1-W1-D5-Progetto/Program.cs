08de7a3233ca007edf8fa4133642a8162ab268c2
EarthDragon10 <lagudadev@gmail.com>
2022-11-25 15:00:37 +0100

    Add Storico Contribuenti

--- B-U1-W1-D5-Progetto/Program.cs
+++ B-U1-W1-D5-Progetto/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace B_U1_W1_D5_Progetto
@@ -10,137 +11,194 @@ namespace B_U1_W1_D5_Progetto
     {
         static void Main(string[] args)
         {
-            Contribuente contribuente = new Contribuente();
             Contribuente.MostraAzioniSportello();
         }
 
         public class Contribuente
         {
-            //private static List<Utente> listUtents = new List<Utente>();
-            public static string Nome { get; set; }
-            public static string Cognome { get; set; }
-            public static string DataNascita { get; set; }
-
-            public static string CodiceFiscale { get; set; }
-            public static string Sesso { get; set; }
-            public static string ComuneResidenza { get; set; }
-
-            public static double RedditoAnnuale { get; set; }
-
-            public static double ImportoDaVersare { get; set; }
+            private static List<Utente> listUtents = new List<Utente>();
 
             public static void MostraAzioniSportello() {
                 try
                 {
                     Console.WriteLine("=========== OPERAZIONI =============");
                     Console.WriteLine("Scegli l'operazione da effettuare");
-                    Console.WriteLine("1. Avviare il processo di inserimento dati per il calcolo dell'imposta sul reddito");           
-                    Console.WriteLine("2. Esci");
+                    Console.WriteLine("1. Avviare il processo di inserimento dati per il calcolo dell'imposta sul reddito");
+                    Console.WriteLine("2. Storico Contribuenti");
+                    Console.WriteLine("3. Esci");
                     Console.WriteLine("====================================\n");
 
                     int scelta = int.Parse(Console.ReadLine());
 
+                    if(scelta <= 0 || scelta > 3 ) {
+                        Console.WriteLine("Hai inserito una scelta non esistente.\n");
+                        Contribuente.MostraAzioniSportello();
+                    }
+
                     switch (scelta)
                     {
                         case 1:
-                            getDatiContribuente();
-                            break;                   
+                            Contribuente.getDatiContribuente();
+                            break;
                         case 2:
-                            Console.WriteLine("\nFine Operazioni");
-                            Console.ReadLine();
+                            StoricoContribuenti();
+                            break;
+                        case 3:
+                            Console.WriteLine("\nFine Operazioni");                           
+                            Environment.Exit(0);
                             break;
                         default:
                             break;
                     }
+       
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"\n{ex.Message}");
                     MostraAzioniSportello();
                 }
             }
 
             public static void getDatiContribuente() {
-              
+
+                Utente utente = new Utente();
+
+                utente.dataNascita = new DataNascita();
 
                 Console.WriteLine("Inizio Processo");
                 Console.WriteLine("\nInserire il suo nome e cognome in questo ordine:");
-
+ 
                 Console.WriteLine("\nNome");
-                Contribuente.Nome = Console.ReadLine();
+                utente.Nome = Console.ReadLine();
+
 
                 Console.WriteLine("\nCognome");
-                Contribuente.Cognome = Console.ReadLine();
+                utente.Cognome = Console.ReadLine();
+
 
                 Console.WriteLine("\nData di nascita");
-                Contribuente.DataNascita = Console.ReadLine();
+                Console.WriteLine("Inserire il giorno");
+                utente.dataNascita.Giorno = Console.ReadLine();
+                Console.WriteLine("Inserire il mese");
+                utente.dataNascita.Mese = Console.ReadLine();
+                Console.WriteLine("Inserire l'anno");
+                utente.dataNascita.Anno = Console.ReadLine();
+
 
                 Console.WriteLine("\nCodice Fiscale");
-                Contribuente.CodiceFiscale = Console.ReadLine();
+                utente.CodiceFiscale = Console.ReadLine();
+
 
                 Console.WriteLine("\nSesso");
-                Contribuente.Sesso = Console.ReadLine();
+                utente.Sesso = Console.ReadLine();
+
 
                 Console.WriteLine("\nComune di Residenza");
-                Contribuente.ComuneResidenza = Console.ReadLine();
+                utente.ComuneResidenza = Console.ReadLine();
+
 
                 Console.WriteLine("\nReddito Annuale");
-                Contribuente.RedditoAnnuale = int.Parse(Console.ReadLine());
+                utente.RedditoAnnuale = int.Parse(Console.ReadLine());
 
+
+                listUtents.Add(utente);
                 Contribuente.CalcoloImpostaReddito();
             }
             public static void CalcoloImpostaReddito() {
+                int ultimoUtente = listUtents.Count - 1;
 
 
-                if(Contribuente.RedditoAnnuale > 0 && Contribuente.RedditoAnnuale <= 15000)
+                if (listUtents[ultimoUtente].RedditoAnnuale > 0 && listUtents[ultimoUtente].RedditoAnnuale <= 15000)
                 {
-                    Contribuente.ImportoDaVersare = (Contribuente.RedditoAnnuale * 23) / 100;
+                    listUtents[ultimoUtente].ImpostaDaVersare = (listUtents[ultimoUtente].RedditoAnnuale * 23) / 100;
 
-                } else if(Contribuente.RedditoAnnuale > 15001 && Contribuente.RedditoAnnuale <= 28000) {
+                } else if (listUtents[ultimoUtente].RedditoAnnuale > 15001 && listUtents[ultimoUtente].RedditoAnnuale <= 28000) {
 
-                    Contribuente.ImportoDaVersare = (((Contribuente.RedditoAnnuale - 15000) * 27) / 100) + 3450;
+                    listUtents[ultimoUtente].ImpostaDaVersare = (((listUtents[ultimoUtente].RedditoAnnuale - 15000) * 27) / 100) + 3450;
 
-                } else if (Contribuente.RedditoAnnuale > 28001 && Contribuente.RedditoAnnuale <= 55000) {
+                } else if (listUtents[ultimoUtente].RedditoAnnuale > 28001 && listUtents[ultimoUtente].RedditoAnnuale <= 55000) {
 
-                    Contribuente.ImportoDaVersare = (((Contribuente.RedditoAnnuale - 28000) * 38) / 100) + 6960;
+                    listUtents[ultimoUtente].ImpostaDaVersare = (((listUtents[ultimoUtente].RedditoAnnuale - 28000) * 38) / 100) + 6960;
 
-                } else if (Contribuente.RedditoAnnuale > 55001 && Contribuente.RedditoAnnuale <= 75000)
-                {
-
-                    Contribuente.ImportoDaVersare = (((Contribuente.RedditoAnnuale - 55000) * 41) / 100) + 17220;
-
-                } else if (Contribuente.RedditoAnnuale > 75001)
+                } else if (listUtents[ultimoUtente].RedditoAnnuale > 55001 && listUtents[ultimoUtente].RedditoAnnuale <= 75000)
                 {
 
-                    Contribuente.ImportoDaVersare = (((Contribuente.RedditoAnnuale - 75000) * 43) / 100) + 25420;
+                    listUtents[ultimoUtente].ImpostaDaVersare = (((listUtents[ultimoUtente].RedditoAnnuale - 55000) * 41) / 100) + 17220;
+
+                } else if (listUtents[ultimoUtente].RedditoAnnuale > 75001)
+                {
+
+                    listUtents[ultimoUtente].ImpostaDaVersare = (((listUtents[ultimoUtente].RedditoAnnuale - 75000) * 43) / 100) + 25420;
 
                 }
                 Contribuente.MostraRisultatiContribuente();
             }
 
-            public static void MostraRisultatiContribuente () {
-                Console.WriteLine("======================================");
-                Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE \n\n");
-                Console.WriteLine($"Contribuente: {Contribuente.Nome} {Contribuente.Cognome}");
-                Console.WriteLine($"Nato il {Contribuente.DataNascita}");
-                Console.WriteLine($"Residente in {Contribuente.ComuneResidenza}");
-                Console.WriteLine($"Codice Fiscale: {Contribuente.CodiceFiscale} \n\n");
-                Console.WriteLine($"Reddito dichiarato: {Contribuente.RedditoAnnuale} \n\n");
-                Console.WriteLine($"Importo da versare {Contribuente.ImportoDaVersare}");
-                Console.WriteLine("====================================\n");
+            public static void MostraRisultatiContribuente() {
+
+                try
+                {
+                    int ultimoUtente = listUtents.Count - 1;
+
+                    Console.OutputEncoding = System.Text.Encoding.UTF8;
+                    Console.WriteLine("======================================");
+                    Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE \n\n");
+                    Console.WriteLine($"Contribuente: {listUtents[ultimoUtente].Nome} {listUtents[ultimoUtente].Cognome}");
+                    Console.WriteLine($"Nato il {listUtents[ultimoUtente].dataNascita.Giorno}/{listUtents[ultimoUtente].dataNascita.Mese}/{listUtents[ultimoUtente].dataNascita.Anno}");
+                    Console.WriteLine($"Residente in {listUtents[ultimoUtente].ComuneResidenza}");
+                    Console.WriteLine($"Codice Fiscale: {listUtents[ultimoUtente].CodiceFiscale} \n\n");
+                    Console.WriteLine($"Reddito dichiarato: {listUtents[ultimoUtente].RedditoAnnuale} \n\n");
+                    Console.WriteLine($"Importo da versare: {listUtents[ultimoUtente].ImpostaDaVersare:C}");
+                    Console.WriteLine("====================================\n");
+                    
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                } finally
+                {
+                    Contribuente.MostraAzioniSportello();
+                }
+
+            }
+
+            public static void StoricoContribuenti()
+            {
+                try
+                {
+                    foreach (Utente item in listUtents)
+                    {
+                        Console.OutputEncoding = System.Text.Encoding.UTF8;
+                        Console.WriteLine("======================================");
+                        Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE \n\n");
+                        Console.WriteLine($"Contribuente: {item.Nome} {item.Cognome}");
+                        Console.WriteLine($"Nato il {item.dataNascita.Giorno}/{item.dataNascita.Mese}/{item.dataNascita.Anno}");
+                        Console.WriteLine($"Residente in {item.ComuneResidenza}");
+                        Console.WriteLine($"Codice Fiscale: {item.CodiceFiscale} \n\n");
+                        Console.WriteLine($"Reddito dichiarato: {item.RedditoAnnuale} \n\n");
+                        Console.WriteLine($"Importo da versare: {item.ImpostaDaVersare:C}");
+                        Console.WriteLine("====================================\n");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                } finally {
+                    Thread.Sleep(2000);
+                    Contribuente.MostraAzioniSportello();
+                }
+
             }
         }
 
         public class Utente
         {
-            public static string Nome { get; set; }
-            public static string Cognome { get; set; }
-            public static DataNascita dataNascita;
-            public static string CodiceFiscale { get; set; }
-            public static string Sesso { get; set; }
-            public static string ComuneResidenza { get; set; }
-            public static double RedditoAnnuale { get; set; }
-            public static double ImpostaDaVersare { get; set; }
+            public string Nome { get; set; }
+            public string Cognome { get; set; }
+            public DataNascita dataNascita;
+            public string CodiceFiscale { get; set; }
+            public string Sesso { get; set; }
+            public string ComuneResidenza { get; set; }
+            public double RedditoAnnuale { get; set; }
+            public double ImpostaDaVersare { get; set; }
         }
 
         public class DataNascita
